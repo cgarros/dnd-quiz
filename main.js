32df415be9be1767aa18212ee4d755f27007b61d
Jacob Reckhard <jacobreckhard@gmail.com>
2020-01-09 15:11:15 -0700

    added some script

---
+++ main.js
@@ -0,0 +1,14 @@
+function generateQuestion (title, answers) {
+  let innerHtml = `
+    <h2>${title}</h2>
+    ${
+      ''.join(answers.map((a, i) => `<div style="display: inline-flex; width: 100vw;"> <input type="radio" name="gender" value="${i}"> <p> ${a.display} </p> </div>`))
+    }
+  `;
+  document.getElementById("box").innerHTML = innerHtml;
+}
+
+function setQuestion(question) {
+  window.current = question;
+  generateQuestion(question.title, question.answers);
+}
